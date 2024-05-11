884dbebded86c55434becd5710230ae6ddd4ce10
Win10MC <91815754+Win10MC@users.noreply.github.com>
2024-05-11 04:10:59 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -72,7 +72,7 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "PogManAI")
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "yea bro AI")
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
